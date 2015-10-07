98bd9401b17fe0144b8a47f9c1a1a461e3b270f6
unknown <d-sano@d-sano-VAIO.es-domain.e-seikatsu.co.jp>
2015-10-07 22:07:02 +0900

    commit

--- app/common/thymeleaf/ThymeleafRenderer.java
+++ app/common/thymeleaf/ThymeleafRenderer.java
@@ -5,8 +5,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
-
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
@@ -15,120 +13,120 @@ import org.thymeleaf.templateresolver.TemplateResolver;
 import org.thymeleaf.templateresolver.UrlTemplateResolver;
 import org.thymeleaf.util.Validate;
 
+import com.google.inject.Singleton;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import play.Configuration;
 import play.Logger;
 import play.Play;
 import play.twirl.api.Html;
 
-import com.google.inject.Singleton;
-
 @Singleton
 public class ThymeleafRenderer {
 
-	private TemplateEngine engine;
+  private TemplateEngine engine;
 
-	public ThymeleafRenderer() {
-		Configuration config = Play.application().configuration();
-		config = config.getConfig("thymeleaf");
-		Validate.notNull(config, "Thymeleaf setting not exist");
+  public ThymeleafRenderer() {
+    Configuration config = Play.application().configuration();
+    config = config.getConfig("thymeleaf");
+    Validate.notNull(config, "Thymeleaf setting not exist");
 
-		String views = config.getString("views");
-		String suffix = config.getString("suffix");
-		String mode = config.getString("mode");
-		Long ttl = config.getMilliseconds("ttl");
-		String encoding = config.getString("encoding");
-		String include = config.getString("include");
+    String views = config.getString("views");
+    String suffix = config.getString("suffix");
+    String mode = config.getString("mode");
+    Long ttl = config.getMilliseconds("ttl");
+    String encoding = config.getString("encoding");
+    String include = config.getString("include");
 
-		// デフォルトの設定値
-		views = (views == null ? "views/" : views);
-		suffix = (suffix == null ? ".html" : suffix);
-		mode = (mode == null ? "LEGACYHTML5" : mode);
-		ttl = (ttl == null ? 1 * 60 * 1000 : ttl);
-		encoding = (encoding == null ? "UTF-8" : encoding);
-		include = (include == null ? "include/*" : include);
+    // デフォルトの設定値
+    views = (views == null ? "views/" : views);
+    suffix = (suffix == null ? ".html" : suffix);
+    mode = (mode == null ? "LEGACYHTML5" : mode);
+    ttl = (ttl == null ? 1 * 60 * 1000 : ttl);
+    encoding = (encoding == null ? "UTF-8" : encoding);
+    include = (include == null ? "include/*" : include);
 
-		// logs/application.logに設定値をログとして出しておく
-		Logger.info(String.format("thymeleaf.views: %s", views));
-		Logger.info(String.format("thymeleaf.suffix: %s", suffix));
-		Logger.info(String.format("thymeleaf.mode: %s", mode));
-		Logger.info(String.format("thymeleaf.ttl: %d", ttl));
-		Logger.info(String.format("thymeleaf.encoding: %s", encoding));
-		Logger.info(String.format("thymeleaf.include: %s", include));
+    // logs/application.logに設定値をログとして出しておく
+    Logger.info(String.format("thymeleaf.views: %s", views));
+    Logger.info(String.format("thymeleaf.suffix: %s", suffix));
+    Logger.info(String.format("thymeleaf.mode: %s", mode));
+    Logger.info(String.format("thymeleaf.ttl: %d", ttl));
+    Logger.info(String.format("thymeleaf.encoding: %s", encoding));
+    Logger.info(String.format("thymeleaf.include: %s", include));
 
-		// thymeleaf.viewsをURIに変換
-		URI uri = null;
-		try {
-			uri = new URI(views);
-		} catch (Exception e) {
-		}
-		Validate.notNull(uri, "Malformat thymeleaf.views: " + views);
+    // thymeleaf.viewsをURIに変換
+    URI uri = null;
+    try {
+      uri = new URI(views);
+    } catch (Exception e) {
+    }
+    Validate.notNull(uri, "Malformat thymeleaf.views: " + views);
 
-		// URIの形式によってリゾルバを選択
-		TemplateResolver resolver = null;
-		String scheme = uri.getScheme();
-		Logger.debug("uri=【" + uri + "】");
-		Logger.debug("scheme=【" + scheme + "】");
-		Logger.debug("uri.getPath()=【" + uri.getPath() + "】");
+    // URIの形式によってリゾルバを選択
+    TemplateResolver resolver = null;
+    String scheme = uri.getScheme();
+    Logger.debug("uri=【" + uri + "】");
+    Logger.debug("scheme=【" + scheme + "】");
+    Logger.debug("uri.getPath()=【" + uri.getPath() + "】");
 
-		if (scheme == null) {
-			resolver = new ClassLoaderTemplateResolver();
-			Logger.debug("======ClassLoaderTemplateResolver");
-			resolver.setPrefix(uri.getPath());
-		} else if (scheme.equals("file")) {
-			resolver = new FileTemplateResolver();
-			Logger.debug("======FileTemplateResolver");
-			resolver.setPrefix(uri.getPath());
-		} else {
-			resolver = new UrlTemplateResolver();
-			Logger.debug("======UrlTemplateResolver");
-			resolver.setPrefix(uri.toASCIIString());
-		}
+    if (scheme == null) {
+      resolver = new ClassLoaderTemplateResolver();
+      Logger.debug("======ClassLoaderTemplateResolver");
+      resolver.setPrefix(uri.getPath());
+    } else if (scheme.equals("file")) {
+      resolver = new FileTemplateResolver();
+      Logger.debug("======FileTemplateResolver");
+      resolver.setPrefix(uri.getPath());
+    } else {
+      resolver = new UrlTemplateResolver();
+      Logger.debug("======UrlTemplateResolver");
+      resolver.setPrefix(uri.toASCIIString());
+    }
 
-		// リゾルバの共通設定
-		resolver.setTemplateMode(mode);
-		resolver.setSuffix(suffix);
-		resolver.setCacheTTLMs(ttl);
-		resolver.setCharacterEncoding(encoding);
-		Set<String> nonChacheablePatterns = new HashSet<String>();
-		nonChacheablePatterns.add(include);
-		resolver.setNonCacheablePatterns(nonChacheablePatterns);
+    // リゾルバの共通設定
+    resolver.setTemplateMode(mode);
+    resolver.setSuffix(suffix);
+    resolver.setCacheTTLMs(ttl);
+    resolver.setCharacterEncoding(encoding);
+    Set<String> nonChacheablePatterns = new HashSet<String>();
+    nonChacheablePatterns.add(include);
+    resolver.setNonCacheablePatterns(nonChacheablePatterns);
 
-		// テンプレートエンジンの作成
-		engine = new TemplateEngine();
-		engine.setTemplateResolver(resolver);
-		engine.addDialect(new LayoutDialect());
-	}
+    // テンプレートエンジンの作成
+    engine = new TemplateEngine();
+    engine.setTemplateResolver(resolver);
+    engine.addDialect(new LayoutDialect());
+  }
 
-	public void clearTemplateCache() {
-		engine.clearTemplateCache();
-	}
+  public void clearTemplateCache() {
+    engine.clearTemplateCache();
+  }
 
-	public Html render(final String templatePath) {
-		return render(templatePath, null, null);
-	}
+  public Html render(final String templatePath) {
+    return render(templatePath, null, null);
+  }
 
-	public Html render(final String templatePath, final Object variable) {
-		return render(templatePath, variable, null);
-	}
+  public Html render(final String templatePath, final Object variable) {
+    return render(templatePath, variable, null);
+  }
 
-	public Html render(final String templatePath,
-			final Map<String, ?> additionalVariables) {
-		return render(templatePath, null, additionalVariables);
-	}
+  public Html render(final String templatePath, final Map<String, ?> additionalVariables) {
+    return render(templatePath, null, additionalVariables);
+  }
 
-	public Html render(final String templatePath, final Object variable,
-			final Map<String, ?> additionalVariables) {
-		Context ctx = new Context();
-		if (variable != null) {
-			ctx.setVariable("it", variable);
-		}
-		if (additionalVariables != null && additionalVariables.size() > 0) {
-			ctx.setVariables(additionalVariables);
-		}
-		return render(templatePath, ctx);
-	}
+  public Html render(final String templatePath, final Object variable,
+      final Map<String, ?> additionalVariables) {
+    Context ctx = new Context();
+    if (variable != null) {
+      ctx.setVariable("it", variable);
+    }
+    if (additionalVariables != null && additionalVariables.size() > 0) {
+      ctx.setVariables(additionalVariables);
+    }
+    return render(templatePath, ctx);
+  }
 
-	public Html render(final String templatePath, Context ctx) {
-		return Html.apply(engine.process(templatePath, ctx));
-	}
+  public Html render(final String templatePath, Context ctx) {
+    return Html.apply(engine.process(templatePath, ctx));
+  }
 }
